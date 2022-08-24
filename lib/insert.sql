2e9425f7a14de56bd3cb5475a9d21cb6102bbbf3
michael muliro <michael.muliro@student.moringaschool.com>
2022-08-24 13:52:42 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+INSERT INTO bears VALUES (1,"Talespin", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Baloo", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"christy", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Miky", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Marlina", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Linda", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
